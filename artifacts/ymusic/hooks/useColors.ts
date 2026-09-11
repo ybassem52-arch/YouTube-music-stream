2b16e46c96873e9480121d0acf9a8b2bb14444fb
ybassem52-arch <ybassem52@gmail.com>
2026-09-12 00:25:05 +0300

    Delete artifacts directory

--- artifacts/ymusic/hooks/useColors.ts
+++
@@ -1,8 +0,0 @@
-import { useColorScheme } from "react-native";
-import colors from "@/constants/colors";
-
-export function useColors() {
-  const scheme = useColorScheme();
-  const palette = scheme === "dark" ? colors.dark : colors.light;
-  return { ...palette, radius: colors.radius };
-}
